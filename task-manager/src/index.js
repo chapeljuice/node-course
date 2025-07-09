ebebc47763c9aa07c2e1f6ba461f10bbffb69715
Ryan Chapel <chaprtc@gmail.com>
2025-07-09 11:10:51 -0700

    Adding more CRUD operations

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -45,6 +45,40 @@ app.get('/users/:id', async (req, res) => {
         res.status(500).send(error);
     }
 });
+
+// Update a user by id
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+    if (!isValidOperation) {
+        const invalidFields = updates.filter((update) => !allowedUpdates.includes(update));
+        return res.status(400).send({ error: 'The following fields are not allowed to be updated: ' + invalidFields.join(', ') });
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// Delete a user by id
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
 // END USERS API
 
 
@@ -77,6 +111,40 @@ app.get('/tasks/:id', async (req, res) => {
 
     try {
         const task = await Task.findById(_id);
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// Update a task by id
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'completed'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+    if (!isValidOperation) {
+        const invalidFields = updates.filter((update) => !allowedUpdates.includes(update));
+        return res.status(400).send({ error: 'The following fields are not allowed to be updated: ' + invalidFields.join(', ') });
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// Delete a task by id
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
         if (!task) {
             return res.status(404).send();
         }
